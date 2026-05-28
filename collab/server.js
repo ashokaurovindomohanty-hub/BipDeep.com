94f641a868659a57da72536dea654b1c575426b7
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-28 20:23:28 +0530

    Delete collab directory

--- collab/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-app.use(express.static('public'));
-server.listen(3002, () => console.log('Collab server listening on port 3002'));
